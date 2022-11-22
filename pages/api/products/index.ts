e117aa366b879ddc55cf9953e7eb0d3625075f14
Leandro Roldan <leandrom.roldan@gmail.com>
2022-11-22 11:47:21 -0300

    method not methods in products/index

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,15 +1,18 @@
-import methods from "micro-method-router";
+import method from "micro-method-router";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getAllProducts } from "controllers/product";
 
 //Gets all the products
-module.exports = methods({
-	async get(req: NextApiRequest, res: NextApiResponse) {
-		try {
-			const products = await getAllProducts(req);
-			res.status(200).send(products);
-		} catch (error) {
-			res.status(400).send({ error: error });
-		}
-	},
+async function getHandler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const products = await getAllProducts(req);
+		res.status(200).send(products);
+	} catch (error) {
+		res.status(400).send({ error: error });
+	}
+}
+const handler = method({
+	get: getHandler,
 });
+
+export default handler;
