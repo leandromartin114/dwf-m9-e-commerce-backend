676807d7cbe6639acc72c79a478f7aa151af36ef
Leandro Roldan <leandrom.roldan@gmail.com>
2022-11-26 12:11:11 -0300

    add cors middleware

--- lib/middlewares.ts
+++ lib/middlewares.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextRequest } from "next/server";
 import parseBearerToken from "parse-bearer-token";
 import { decodeToken } from "./jwt";
 import * as yup from "yup";
@@ -55,3 +56,24 @@ export function queryAndBodyMid(
 		}
 	};
 }
+
+export function middleware(req: NextRequest) {
+	if (req.method == "OPTIONS") {
+		return new Response(null, {
+			status: 204,
+			headers: {
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+				"Access-Control-Allow-Headers":
+					req.headers.get("Access-Control-Request-Headers") || "",
+				Vary: "Access-Control-Request-Headers",
+				"Content-Length": "0",
+			},
+		});
+	}
+}
+
+export const config = {
+	matcher: ["/api/:path*"],
+};
